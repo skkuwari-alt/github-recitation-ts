98a3eb0ba4204fc42936e9476c1da379e8d66a18
Salwa Al-Kuwari <skkuwari@andrew.cmu.edu>
2025-09-01 20:15:03 +0300

    Fix export in fib.ts so fibRoute.ts can import fibonacci

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,13 @@
 // util function that computes the fibonacci numbers
-export = function fibonacci(n: number): number{
+export function fibonacci(n: number): number{
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
+
